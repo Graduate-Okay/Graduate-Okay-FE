9a807bafd376be1c05a1f6ef7b2e6484d8815c65
pangkyu <baesense@naver.com>
2023-10-14 22:16:04 +0900

    yarn add react-router-dom styled-components styled-reset

---
+++ graduate-okay-fe/src/pages/Main.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Main: React.FC = () => {
+  return (
+    <>
+      <main className="MainPage--box">
+        <img
+          className="contentsPage-img"
+          src="imgs/background.jpg"
+          alt="background"
+        ></img>
+      </main>
+    </>
+  );
+};
+export default Main;
