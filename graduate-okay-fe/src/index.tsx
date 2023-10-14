b4567a0c30d74ce09a4c08ac624092ec82bd4d22
pangkyu <baesense@naver.com>
2023-10-14 22:25:30 +0900

    Design : css 초기화
    크로스브라우징 방지

--- graduate-okay-fe/src/index.tsx
+++ graduate-okay-fe/src/index.tsx
@@ -1,12 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import reset from "styled-reset";
+import { createGlobalStyle } from "styled-components";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+`;
 root.render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>
 );
