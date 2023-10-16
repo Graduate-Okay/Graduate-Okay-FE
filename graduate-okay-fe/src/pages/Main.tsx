533f92e6b9463a09ee6383fd9b69670fd59119c2
pangkyu <baesense@naver.com>
2023-10-16 12:43:38 +0900

    Design : 메인페이지 UI

--- graduate-okay-fe/src/pages/Main.tsx
+++ graduate-okay-fe/src/pages/Main.tsx
@@ -1,24 +1,27 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { ThemeProvider } from "styled-components";
+import theme from "../constants/theme";
 
 const Main: React.FC = () => {
   return (
-    <MainPageDiv>
-      <ImgDiv>
-        <img
-          className="contentsPage-img"
-          src="imgs/background.jpg"
-          alt="background"
-        />
-      </ImgDiv>
-    </MainPageDiv>
+    <ThemeProvider theme={theme}>
+      <MainPageDiv>
+        <ImgDiv>
+          <img
+            className="contentsPage-img"
+            src="imgs/background.jpg"
+            alt="background"
+          />
+        </ImgDiv>
+      </MainPageDiv>
+    </ThemeProvider>
   );
 };
 export default Main;
 
 const MainPageDiv = styled.div`
   width: 100%;
-  height: 100%;
+  height: 70vh;
   margin-top: 1.2rem;
   margin-bottom: 1.2rem;
 `;
@@ -26,6 +29,20 @@ const MainPageDiv = styled.div`
 const ImgDiv = styled.div`
   display: flex;
   width: 100%;
-  height: 40rem;
+  height: 70%;
   justify-content: center;
+  > img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+  @media ${({ theme }) => theme.device.laptop} {
+    width: 80%;
+    height: 80%;
+    margin: 0 auto;
+    margin-top: 5vh;
+  }
+  @media ${({ theme }) => theme.device.largeLaptop} {
+    margin-top: 3vh;
+  }
 `;
