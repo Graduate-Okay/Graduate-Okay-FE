71dd250048aee3a064acf7013691d7287804d19b
pangkyu <baesense@naver.com>
2023-10-23 17:26:13 +0900

    Feat : 뒤로 가기 버튼

--- graduate-okay-fe/src/components/Header.tsx
+++ graduate-okay-fe/src/components/Header.tsx
@@ -2,16 +2,28 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import styled, { ThemeProvider } from "styled-components";
 import theme from "../constants/theme";
+import BackButton from "../assets/imgs/BackButton.svg";
 
 const Header: React.FC = () => {
   const navigate = useNavigate();
   const moveMain = () => {
     navigate("/");
   };
+
   return (
     <ThemeProvider theme={theme}>
       <HeaderDiv>
+        <SubDiv>
+          <img src={BackButton} alt="뒤로가기" onClick={() => navigate(-1)} />
+        </SubDiv>
         <img src="imgs/logo.png" alt="헤더로고" onClick={moveMain} />
+        <SubDiv
+          onClick={() => {
+            alert("준비중입니다");
+          }}
+        >
+          <p>로그인</p>
+        </SubDiv>
       </HeaderDiv>
     </ThemeProvider>
   );
@@ -22,8 +34,24 @@ export default Header;
 const HeaderDiv = styled.header`
   display: flex;
   align-content: center;
-  justify-content: center;
-  margin-top: 1.2rem;
+  justify-content: space-around;
   width: 100%;
   height: 9vh;
+  margin-top: 1.2rem;
+
+  @media ${({ theme }) => theme.device.tablet} {
+    width: 80%;
+    margin: 1.2rem auto;
+  }
+`;
+
+const SubDiv = styled.div`
+  display: flex;
+  > p {
+    font-size: 1.4rem;
+    margin: auto 0;
+  }
+  > img {
+    width: 3.8rem;
+  }
 `;
