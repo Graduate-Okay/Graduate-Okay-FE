d1aecbb8029ad7ef8ba49aa104a9c96b0cd9f963
pangkyu <baesense@naver.com>
2023-11-09 20:31:40 +0900

    Feat : lazy loading 적용

--- graduate-okay-fe/src/App.tsx
+++ graduate-okay-fe/src/App.tsx
@@ -1,21 +1,21 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import { useCookies } from "react-cookie";
-import Header from "./components/Header";
-import Nav from "./components/Nav";
-import Footer from "./components/footer/Footer";
-import Main from "./pages/Main";
-import Notice from "./pages/Notice";
-import KyRecommend from "./pages/KyRecommend";
-import Graduate from "./pages/Graduate";
-import Mypage from "./pages/Mypage";
-import Login from "./pages/Login";
 import RouteChangeTracker from "./utils/RouteChangeTracker";
-import NoticeDetail from "./pages/NoticeDetail";
-import Signup from "./pages/Signup";
-import Find from "./pages/Find";
-import KyRecommendDetail from "./pages/KyRecommendDetail";
-import ModifyInfo from "./pages/ModifyInfo";
+const Header = React.lazy(() => import("./components/Header"));
+const Nav = React.lazy(() => import("./components/Nav"));
+const Footer = React.lazy(() => import("./components/footer/Footer"));
+const Notice = React.lazy(() => import("./pages/Notice"));
+const Main = React.lazy(() => import("./pages/Main"));
+const KyRecommend = React.lazy(() => import("./pages/KyRecommend"));
+const Graduate = React.lazy(() => import("./pages/Graduate"));
+const Mypage = React.lazy(() => import("./pages/Mypage"));
+const Login = React.lazy(() => import("./pages/Login"));
+const NoticeDetail = React.lazy(() => import("./pages/NoticeDetail"));
+const Signup = React.lazy(() => import("./pages/Signup"));
+const Find = React.lazy(() => import("./pages/Find"));
+const KyRecommendDetail = React.lazy(() => import("./pages/KyRecommendDetail"));
+const ModifyInfo = React.lazy(() => import("./pages/ModifyInfo"));
 
 function App() {
   const [cookies] = useCookies(["accessToken"]);
@@ -23,54 +23,60 @@ function App() {
   return (
     <BrowserRouter>
       <RouteChangeTracker />
-      <Header />
-      <Nav />
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/notice" element={<Notice />} />
-        <Route path="/notice/:id" element={<NoticeDetail />} />
-        <Route path="/KyRecommend" element={<KyRecommend />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/find" element={<Find />} />
-        <Route
-          path="/KyRecommend/:id"
-          element={
-            cookies.accessToken ? (
-              <KyRecommendDetail />
-            ) : (
-              <Navigate replace to="/login" />
-            )
-          }
-        />
-        <Route
-          path="/Graduate"
-          element={
-            cookies.accessToken ? (
-              <Graduate />
-            ) : (
-              <Navigate replace to="/login" />
-            )
-          }
-        />
-        <Route
-          path="/Mypage"
-          element={
-            cookies.accessToken ? <Mypage /> : <Navigate replace to="/login" />
-          }
-        />
-        <Route
-          path="/mypage/modifyInfo"
-          element={
-            cookies.accessToken ? (
-              <ModifyInfo />
-            ) : (
-              <Navigate replace to="/login" />
-            )
-          }
-        />
-      </Routes>
-      <Footer />
+      <Suspense fallback={<div>로딩중...</div>}>
+        <Header />
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/notice" element={<Notice />} />
+          <Route path="/notice/:id" element={<NoticeDetail />} />
+          <Route path="/KyRecommend" element={<KyRecommend />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/find" element={<Find />} />
+          <Route
+            path="/KyRecommend/:id"
+            element={
+              cookies.accessToken ? (
+                <KyRecommendDetail />
+              ) : (
+                <Navigate replace to="/login" />
+              )
+            }
+          />
+          <Route
+            path="/Graduate"
+            element={
+              cookies.accessToken ? (
+                <Graduate />
+              ) : (
+                <Navigate replace to="/login" />
+              )
+            }
+          />
+          <Route
+            path="/Mypage"
+            element={
+              cookies.accessToken ? (
+                <Mypage />
+              ) : (
+                <Navigate replace to="/login" />
+              )
+            }
+          />
+          <Route
+            path="/mypage/modifyInfo"
+            element={
+              cookies.accessToken ? (
+                <ModifyInfo />
+              ) : (
+                <Navigate replace to="/login" />
+              )
+            }
+          />
+        </Routes>
+        <Footer />
+      </Suspense>
     </BrowserRouter>
   );
 }
