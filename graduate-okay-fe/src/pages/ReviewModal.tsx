674462ae5a348f83b01161b9ecec393a31448967
pangkyu <baesense@naver.com>
2023-12-10 00:35:00 +0900

    Design : 모달 디자인

--- graduate-okay-fe/src/pages/ReviewModal.tsx
+++ graduate-okay-fe/src/pages/ReviewModal.tsx
@@ -6,7 +6,17 @@ const ReviewModal: React.FC = () => {
   return (
     <ThemeProvider theme={theme}>
       <ModalSection>
-        <p>sdf</p>
+        <ModalDiv>
+          <ModalHeader>
+            <ReviewHeader>리뷰 작성</ReviewHeader>
+            <ReviewClose>🗙</ReviewClose>
+          </ModalHeader>
+          <ModalContent>
+            <p>sdf</p>
+            <p>sdf</p>
+            <p>sdf</p>
+          </ModalContent>
+        </ModalDiv>
       </ModalSection>
     </ThemeProvider>
   );
@@ -15,10 +25,44 @@ const ReviewModal: React.FC = () => {
 export default ReviewModal;
 
 const ModalSection = styled.section`
-  display: absolute;
-  top: 0;
-  left: 0;
-  width: 100vw;
+  width: 100%;
   height: 100vh;
-  z-index: 999;
+`;
+
+const ModalDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  position: fixed;
+  width: 280px;
+  height: 300px;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: white;
+  border: 1px solid black;
+  border-radius: 10px;
+  align-items: center;
+`;
+
+const ModalHeader = styled.div`
+  display: flex;
+  font-size: 1.6rem;
+  width: 100%;
+  height: 4vh;
+  align-items: center;
+`;
+const ReviewHeader = styled(ModalHeader)`
+  justify-content: center;
+`;
+
+const ReviewClose = styled(ModalHeader)`
+  justify-content: right;
+  width: 5%;
+  &:hover {
+    opacity: 0.5;
+  }
+`;
+
+const ModalContent = styled.div`
+  display: flex;
 `;
